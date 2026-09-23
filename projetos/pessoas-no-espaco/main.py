f658a5b75b097a9ceb1ad8425c7fd889eb18147a
Ricardo Marchesi <ricacomarchesi@gmail.com>
2026-09-23 11:44:45 -0300

    projeto 1: adiciona tratamento de erro para chamada a API externa

--- projetos/pessoas-no-espaco/main.py
+++ projetos/pessoas-no-espaco/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
@@ -7,16 +7,21 @@ OPEN_NOTIFY_URL = "http://api.open-notify.org/astros.json"
 
 @app.get("/")
 def raiz():
-    return {"message": "API de pessoas no espaço. Acesse /pessoas-no-espaco"}
+    return {"mensagem": "API de pessoas no espaço. Acesse /pessoas-no-espaco"}
 
 @app.get("/pessoas-no-espaco")
 def pessoas_no_espaco():
-   
+    try:
         resposta = requests.get(OPEN_NOTIFY_URL, timeout=5)
-        dados = resposta.json()
+        resposta.raise_for_status()
+    except requests.exceptions.RequestException:
+        raise HTTPException(
+            status_code=503,
+            detail="Serviço indisponível. Tente novamente mais tarde."
+        )
 
-        return {
-            "quantidade": dados["number"],
-            "astronautas": [pessoa["name"] for pessoa in dados["people"]]
-        }
-
+    dados = resposta.json()
+    return {
+        "quantidade": dados["number"],
+        "astronautas": [pessoa["name"] for pessoa in dados["people"]]
+    }
